a07dc10b4d92111a525923072e3a0991b9d65d28
g <bhj>
2018-12-20 13:24:29 +0300

    jn

--- index.js
+++ index.js
@@ -5,9 +5,13 @@ const TOKEN = process.env.TLGTOKEN
 const bot = new TelegramBot(TOKEN, {polling: true})
 
 bot.onText(/\/test/, (msg, match) => {
-	bot.sendMessage(msg.chat.id , `test success`)
+	bot.sendMessage(`@puteshestvuyseichas` , `test success`)
 })
 
 bot.on('message', msg => {
   bot.sendMessage(msg.chat.id, `Hello, ${msg.from.first_name}`)
 })
+
+
+
+
